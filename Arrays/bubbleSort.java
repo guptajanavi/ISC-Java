bf10542f86c5d5869e5feff7428e2f1041185a4d
guptajanavi <79553848+guptajanavi@users.noreply.github.com>
2021-05-10 18:45:50 +0530

    Update bubbleSort.java

--- Arrays/bubbleSort.java
+++ Arrays/bubbleSort.java
@@ -5,7 +5,8 @@ class bubbleSort{
         int a[]=new int[5];
         int i,j,t;
         System.out.println("Enter 5 numbers ");
-        for(i=0;i<a.length;i++){                                          a[i]=sc.nextInt();                              
+        for(i=0;i<a.length;i++){                                          
+            a[i]=sc.nextInt();                              
         }
         for(i=0;i<a.length;i++){
             for(j=0;j<a.length-1-i;j++){
